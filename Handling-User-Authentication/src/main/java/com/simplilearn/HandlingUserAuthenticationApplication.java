544d5c7d80a3baf67741a3661d891f8d4701c611
RavikantX <ravikant97087@gmail.com>
2022-03-12 16:07:02 +0530

    Project-3__Phase-3

---
+++ Handling-User-Authentication/src/main/java/com/simplilearn/HandlingUserAuthenticationApplication.java
@@ -0,0 +1,14 @@
+package com.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class HandlingUserAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandlingUserAuthenticationApplication.class, args);
+	}
+
+}
